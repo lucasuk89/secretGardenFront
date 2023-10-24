4355ed25d01fc635a1f819798cd7177de71ed410
Lucas Ferraz Martins <lucasuk89@gmail.com>
2023-10-24 13:13:24 +0200

    Logout inserido, scroll vertical no dashboard...

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { Link, Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import { NavDropdown } from 'react-bootstrap';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
@@ -11,29 +12,34 @@ function App() {
 
   const handleLogout = () => {
     setIsLoggedIn(false);
-    Navigate("/api/login");
+
   };
 
+  let user = JSON.parse(localStorage.getItem('user'))
+  
   return (
     <div>
       <Router>
-        <div>
-          <nav>
-            <ul>
-              <li><Link to="/home">Home</Link></li>
-              <li><Link to="/register">Register</Link></li>
-              {isLoggedIn ? (
-                <>
-                  <li><Link to="/dashboard">Dashboard</Link></li>
-                  <li onClick={handleLogout}>Logout</li>
-                </>
-              ) : (
-                <li><Link to="/login">Sign in</Link></li>
-              )}
-            </ul>
-          </nav>
-        </div>
-
+        <nav className="navbar">
+          <ul className="nav-list">
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/register">Register</Link></li>
+            {isLoggedIn ? (
+              <>
+                <li><Link to="/dashboard">Dashboard</Link></li>
+                <li onClick={handleLogout}><Link to="/login">Logout</Link></li>
+            
+              </>
+            ) : (
+              <li><Link to="/login">Sign in</Link></li>
+            )}
+            <li className="user-dropdown">
+            <NavDropdown title={`Welcome, ${user && user.name}`}>
+                <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
+              </NavDropdown>
+            </li>
+          </ul>
+        </nav>
         <div>
           <div className='secretgardenTitle'>
             <h1 className='welcomeTitle'>Welcome to the Secret Garden</h1>
@@ -42,19 +48,14 @@ function App() {
           <h1>A great time to save your memories...or...</h1>
           <p>At the Secret Garden, you can store book titles and authors, as well as craft eerie narratives about your most mysterious and chilling secrets...</p>
         </div>
-        
         <div className="containerImgandTitle">
-       
-        <div className="image">
-          <img src="../public/fairyTale.jpeg" alt="Imagem" />
-          <img src="../public/it.jpeg" alt="Imagem" />
-          <img src="../public/holy.jpeg" alt="Imagem" />
-          <img src="../public/billySummers.jpeg" alt="Imagem" />
+          <div className="image">
+            <img src="../public/fairyTale.jpeg" alt="Imagem" />
+            <img src="../public/it.jpeg" alt="Imagem" />
+            <img src="../public/holy.jpeg" alt="Imagem" />
+            <img src="../public/billySummers.jpeg" alt="Imagem" />
+          </div>
         </div>
-      </div>
-
-
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
